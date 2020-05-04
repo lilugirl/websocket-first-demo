1cdb30887110d2e789a20f902dc56efc3f564c74
lilu <lilugirl2005@126.com>
2020-05-04 13:37:01 +0800

    消息通信演示

--- client/test.js
+++ client/test.js
@@ -3,4 +3,9 @@ const ws = new WebSocket('ws://127.0.0.1:3000')
 
 ws.on('open', function () {
   console.log('client is connected to Server');
+
+  ws.send('client say hell to server!!!')
+  ws.on('message', function (msg) {
+    console.log(msg)
+  })
 })
--- server/index.js
+++ server/index.js
@@ -6,4 +6,12 @@ const wss = new WebSocket.Server({
 
 wss.on('connection', function connection(ws) {
   console.log('one client is connected')
+
+  // 接收客户端的消息
+  ws.on('message', function (msg) {
+    console.log(msg)
+  })
+
+  // 主动发送消息给客户端
+  ws.send('Message from server')
 })
