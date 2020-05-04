c2bc969658a083e60675610fe06ef8f72b5d5952
lilu <lilugirl2005@126.com>
2020-05-04 19:48:09 +0800

    在线人数统计 离开聊天室

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const WebSocket = require('ws')
 
+var num = 0;
+
 const wss = new WebSocket.Server({
   port: 3000
 })
@@ -9,6 +11,13 @@ wss.on('connection', function connection(ws) {
 
   // 接收客户端的消息
   ws.on('message', function (msg) {
+    const msgObj = JSON.parse(msg)
+    if (msgObj.event === 'enter') {
+      ws.name = msgObj.message
+      num++
+
+    }
+
     // console.log(msg)
 
     //主动发送到客户端
@@ -18,11 +27,33 @@ wss.on('connection', function connection(ws) {
     //广播消息
     wss.clients.forEach((client) => {
 
-      // 判断非自己的客户端才发送
-      if (ws !== client && client.readyState === WebSocket.OPEN) {
-        client.send(msg)
+
+      if (client.readyState === WebSocket.OPEN) {
+        msgObj.name = ws.name
+        // msgObj.num = wss.clients.size;
+        msgObj.num = num;
+        client.send(JSON.stringify(msgObj))
       }
 
+    })
+  })
+
+  //当ws客户端断开连接时
+  ws.on('close', function () {
+    if (ws.name) {
+      num--;
+    }
+
+    let msgObj = {}
+    //广播消息
+    wss.clients.forEach((client) => {
+
+      if (client.readyState === WebSocket.OPEN) {
+        msgObj.name = ws.name;
+        msgObj.num = num;
+        msgObj.event = "out";
+        client.send(JSON.stringify(msgObj))
+      }
     })
   })
 
