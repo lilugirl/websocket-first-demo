1c29b83a48c1c5b708ccd6073bb6586775e9ee1b
lilu <lilugirl2005@126.com>
2020-05-04 16:03:46 +0800

    一个聊天室应用雏形

--- server/index.js
+++ server/index.js
@@ -9,7 +9,21 @@ wss.on('connection', function connection(ws) {
 
   // 接收客户端的消息
   ws.on('message', function (msg) {
-    console.log(msg)
+    // console.log(msg)
+
+    //主动发送到客户端
+    //ws.send("Server:" + msg)
+
+
+    //广播消息
+    wss.clients.forEach((client) => {
+
+      // 判断非自己的客户端才发送
+      if (ws !== client && client.readyState === WebSocket.OPEN) {
+        client.send('Server:' + msg)
+      }
+
+    })
   })
 
   // 主动发送消息给客户端
